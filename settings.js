ada0b3df9974e2928df81664d6e441c331cef4a5
HanzBOTZ <100819969+HANZBOTZGZ@users.noreply.github.com>
2022-05-31 06:01:40 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281223918622'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281223918622'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281223918622' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6288215110787','6287830949637'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6288215110787','6287830949637'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6288215110787','6287830949637']//ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'YT RIYO XDZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Minami-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'YT RIYO XDZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Hanz OFC' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'HanzBOTZ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Hanz Jawa' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCq2OIMRWymUW4ykJnfHp5PQ'
-global.ig = 'https://instagram.com/riyoxdz'
-global.mygc = 'https://chat.whatsapp.com/KglIECrtEuJ3yBPo0S5cDR'
-global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'mikumiki683@gmail.com'
-global.region = 'Ciamis, Jawa Barat, Indonesia'
+global.youtube = 'https://youtube.com/channel/UCF0o7qA7XnWxl-9FNY8YfZw'
+global.ig = '-'
+global.mygc = 'https://chat.whatsapp.com/Bu8bFnGAHrQKfNcwaG9hCG'
+global.myweb = 'github.com/HANZBOTZGZ'
+global.email = 'tzyhengker518@gmail.com'
+global.region = 'Pati, Jawa Tengah, Indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Minami-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'YT RIYO XDZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '©Hanz Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'By Hanz OFC' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
